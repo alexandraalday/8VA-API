a6fc9cd42369924b87945e858ca120f408b010b2
alexandraalday <alexandra.alday@gmail.com>
2017-11-29 16:52:46 -0800

    track model completed

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,17 @@
 class Track < ApplicationRecord
+	belongs_to :band
+	belongs_to :genre
+	has_attached_file :mp3,
+					  :url => '/:class/:id/:style.:extension',
+					  :path => ':rails_root/public/:class/:id_partition/:style.:extension'
+
+	validates_attachment :mp3, content_type: { 
+		content_type: [
+  			'application/mp3',
+			'application/x-mp3',
+			'audio/mpeg',
+			['audio/mpeg'], 
+			'audio/mp3'
+		] 
+	}
 end

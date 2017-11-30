95631c08bb26ed3b50b1526c5590da27618ad6e5
alexandraalday <alexandra.alday@gmail.com>
2017-11-29 16:48:49 -0800

    artists model completed

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,14 @@
 class Artist < ApplicationRecord
+	validates :name, presence:true
+	validates :email, presence:true
+	validates :status, presence:true
+	attr_accessor :track_data
+	has_many :tracks, :dependent => :destroy
+	has_many :genres, :through => :tracks
+
+	def save_attachments(params) 
+		params[:track_data].each do |track| 
+			self.tracks.create(:mp3 => track) 
+		end 
+	end
 end

c71b8624b30d281c00253e109479e7a72fb116d3
alexandraalday <alexandra.alday@gmail.com>
2017-11-29 16:52:00 -0800

    scaffold tracks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130004541) do
+ActiveRecord::Schema.define(version: 20171130005102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20171130004541) do
     t.string "name"
     t.string "email"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tracks", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

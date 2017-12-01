312b5335dfb13efbb3a2624eff2bd4a4469c6a6c
alexandraalday <alexandra.alday@gmail.com>
2017-12-01 09:07:52 -0800

    add nested routes and download routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :genres
-  resources :tracks
-  resources :artists
+  resources :tracks do
+  	member do
+  		get :download
+  	end
+  end
+  resources :artists do
+    resources :tracks, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
